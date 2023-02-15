035e64672df6b7a06805a6a8a52e3df2abba5c4f
barmenski <1kiloom@gmail.com>
2023-02-15 20:52:26 +0300

    Create output_lcd.js

---
+++ output_lcd.js
@@ -0,0 +1,12 @@
+// Import the module
+const LCD = require('raspberrypi-liquid-crystal');
+
+// Instantiate the LCD object on bus 1 address 3f with 16 chars width and 2 lines
+const lcd = new LCD(1, 0x27, 16, 2);
+// Init the lcd (must be done before calling any other methods)
+lcd.beginSync();
+// Clear any previously displayed content
+lcd.clearSync();
+// Display text multiline
+lcd.printLineSync(0, 'hello');
+lcd.printLineSync(1, 'world!');
