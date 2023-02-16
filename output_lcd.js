21c1221387b9589343bbe59b260fd6f76571ee93
barmenski <1kiloom@gmail.com>
2023-02-16 21:55:24 +0300

    add: gitignore

--- output_lcd.js
+++ output_lcd.js
@@ -1,7 +1,7 @@
 // Import the module
 const LCD = require('raspberrypi-liquid-crystal');
 
-// Instantiate the LCD object on bus 1 address 3f with 16 chars width and 2 lines
+// Instantiate the LCD object on bus 1 address 27 with 16 chars width and 2 lines
 const lcd = new LCD(1, 0x27, 16, 2);
 // Init the lcd (must be done before calling any other methods)
 lcd.beginSync();
