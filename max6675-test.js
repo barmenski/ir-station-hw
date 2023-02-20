9f2405cfe8fb17d56d580ded033ab9a85fa0043f
barmenski <1kiloom@gmail.com>
2023-02-21 00:57:04 +0300

    Create max6675-test.js

---
+++ max6675-test.js
@@ -0,0 +1,17 @@
+const Max6675 = require("..");
+
+const CS = 4;
+const SCK = 24;
+const SO = [25, 12, 16, 20, 21];
+const UNIT = 1;
+const max6675 = new Max6675();
+max6675.setPin(CS, SCK, SO, UNIT);
+
+(async () => {
+	while (true) {
+		const { temp, unit } = max6675.readTemp();
+		if (temp.length)
+			console.log(`${new Date()}:${temp.map(item => item + unit)}`);
+		await max6675.sleep(2000);
+	}
+})();
