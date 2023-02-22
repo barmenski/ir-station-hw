184c0724da107ef7812ef21024cdc48c24694c31
0barmenski <1kiloom@gmail.com>
2023-02-22 20:32:37 +0300

    fix: max6675-test.js to °C

--- max6675-test.js
+++ max6675-test.js
@@ -2,7 +2,7 @@ const Max6675 = require("max6675-raspi");
 
 const CS = "4";
 const SCK = "24";
-const SO = ["25", "12", "16", "20", "21"];
+const SO = ["25", "12"];
 const UNIT = "1";
 const max6675 = new Max6675();
 max6675.setPin(CS, SCK, SO, UNIT);
@@ -11,7 +11,8 @@ max6675.setPin(CS, SCK, SO, UNIT);
 	while (true) {
 		const { temp, unit } = max6675.readTemp();
 		if (temp.length)
-			console.log(`${new Date()}:${temp.map(item => item + unit)}`);
+			console.log(`unit: ${unit}`);
+			console.log(`${temp.map(item => (item + unit)/4)}°C`);
 		await max6675.sleep(2000);
 	}
 })();
