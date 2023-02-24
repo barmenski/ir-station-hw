5ef98899bad5d79ced8f965b62452b9dca61c90c
barmenski <1kiloom@gmail.com>
2023-02-25 00:39:26 +0300

    edit: index.js:

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
-
 const LCD = require('raspberrypi-liquid-crystal');
-const Max6675 = require("max6675-raspi");
+const Max6675 = require('max6675-raspi');
 
 const lcd = new LCD(1, 0x27, 16, 2);
-// Init the lcd (must be done before calling any other methods)
-lcd.beginSync();
-// Clear any previously displayed content
-//lcd.clearSync();
-// Display text multiline
-//lcd.printLineSync(0, 'Hello, world!');
-//lcd.printLineSync(1, 'I output LCD.');
-
-
-const CS = "4";
-const SCK = "24";
-const SO = ["25", "12"];
-const UNIT = "1";
 const max6675 = new Max6675();
+
+(async () => {
+  await lcd.begin();
+})();
+
+const CS = '4';
+const SCK = '24';
+const SO = ['25', '12'];
+const UNIT = 1;
+
 max6675.setPin(CS, SCK, SO, UNIT);
 
 (async () => {
-	while (true) {
-		const { temp, unit } = max6675.readTemp();
-		if (temp.length)
-			lcd.clearSync();
-			lcd.printLineSync(0, `Temp -:${temp[0]/4}°C`);
-			lcd.printLineSync(1, `Temp _:${temp[1]/4}°C`);
-			//console.log(`unit: ${unit}`);
-			//console.log(`${temp.map(item => (item + unit)/4)}°C`);
-		await max6675.sleep(2000);
-	}
+  while (true) {
+    const { temp, unit } = max6675.readTemp();
+    if (temp.length) await lcd.clear();
+    await lcd.printLine(0, `Temp 1:${temp[0]} ${unit}`);
+    await lcd.printLine(1, `Temp 2:${temp[1]} ${unit}`);
+    await max6675.sleep(1000);
+  }
 })();
+
+async function stop() {
+  const phrase = `Buy-buy!`;
+  await lcd.printLine(0, phrase);
+  for (let i = 0; i < phrase.length; i++) {
+    await lcd.scrollDisplayLeft();
+  }
+  await lcd.clear();
+  await lcd.noDisplay();
+}
+
+process.on('SIGINT', function () {
+  console.log('\nir-station-hw closed');
+  stop();
+  process.exit();
+});
