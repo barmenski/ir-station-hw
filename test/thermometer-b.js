e955b5c4bb9723a93dfa53edb909482b57171b09
barmenski <1kiloom@gmail.com>
2023-10-27 21:11:09 +0300

    try temp measure

--- test/thermometer-b.js
+++ test/thermometer-b.js
@@ -6,9 +6,9 @@ class  Thermometer {
 	this.lcd = new LCD(1, 0x27, 16, 2);
 	this.lcd.beginSync();
 
-	this.CS = '4';
-	this.SCK = '24';
-	this.SO = ['25', '12'];
+	this.CS = '4'; //GPIO4=pin7
+	this.SCK = '24';//GPIO24=pin18
+	this.SO = ['8', '25'];//GPIO8,25=pin24(top),22(bottom)
 	this.UNIT = 1;
 
 	this.max6675 = new Max6675();
@@ -40,8 +40,8 @@ async measure(start) {
 		this.cycle=true;
 		try {
 			await this.lcd.clear();
-			await this.lcd.printLine(0, "Temp1: ");
-			await this.lcd.printLine(1, "Temp2: ");
+			await this.lcd.printLine(0, "Top  : ");
+			await this.lcd.printLine(1, "Bottm: ");
 		} catch(e) {
 			console.log(`Problem with LCD: ${e}`);
 		}
